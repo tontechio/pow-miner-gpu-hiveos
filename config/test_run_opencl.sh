3e79dd254e34870f65e86fd626d2c6705426e989
Dr.Elias <ilya@ton.org>
2021-11-19 18:36:46 +0300

    remove all configs

--- config/test_run_opencl.sh
+++
@@ -1,3 +0,0 @@
-../assets/tonlib-opencl-cli -V
-../assets/tonlib-opencl-cli -v 3 -C global.config.json -e "pminer start Ef-FV4QTxLl-7Ct3E6MqOtMt-RGXMxi27g4I645lw6MTWg0f EQDU86V5wyPrLd4nQ0RHPcCLPZq_y1O5wFWyTsMw63vjXTOv 0 32 1"
-
