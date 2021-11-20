9a6b3ad4cd3a69d2d7d242bb7f821f4dce34a627
Dr.Elias <ilya@ton.org>
2021-11-20 04:26:32 +0300

    move systemd units

--- h-config.sh
+++ h-config.sh
@@ -17,8 +17,7 @@ if [[ -z "$WALLET_ADR" ]]; then
   exit 1
 fi
 
-# READ CONFIG
-TMPFS_LOGS_ENEBLED=$(echo "{$CUSTOM_USER_CONFIG}" | jq -r '.tmpfs_logs_enable')
+# READ MINER KEYS
 MINER_KEYS=$(echo "{$CUSTOM_USER_CONFIG}" | jq '[. | keys[] | select(contains("miner"))]')
 MINER_NUM=$(echo "$MINER_KEYS" | jq 'length')
 
@@ -31,36 +30,5 @@ jq -n \
   --argjson config "{$CUSTOM_USER_CONFIG}" \
   '{"type":$type, "wallet":$wallet, "keys":$keys, "num":$num, $config}' \
   >$EXEC_CONF
-
-# STOP AND REMOVE SYSTEMD UNITS
-systemctl list-units -t service --full | grep tonminer | awk '{print $1}' | xargs -i systemctl stop \{\}
-rm $UNITS_DIR/tonminer*.service
-systemctl daemon-reload
-
-# CREATE SYSTEMD UNITS
-echo $MINER_KEYS | jq -c -r '.[]' | while read KEY; do
-  VARS=$(echo "{$CUSTOM_USER_CONFIG}" | jq "with_entries(select([.key] | inside([\"$KEY\"]))) | .[]")
-  GIVER=$(echo $VARS | jq -r '.[0]')
-  GPU_ID=$(echo $VARS | jq -r '.[1]')
-  BOOST_FACTOR=$(echo $VARS | jq -r '.[2]')
-  PLATFORM_ID=$(echo $VARS | jq -r '.[3]')
-  VERBOSITY=$(echo $VARS | jq -r '.[4]')
-  PARAMETERS=" -s $SCRIPT_DIR/logs/status-tonminer-$KEY.json"
-  if [[ "$TMPFS_LOGS_ENEBLED" == "yes" ]]; then
-    PARAMETERS="$PARAMETERS -l $SCRIPT_DIR/logs/log-tonminer-$KEY"
-  fi
-  UNIT_FILE="$UNITS_DIR/tonminer-$KEY.service"
-  echo "$KEY: $UNIT_FILE"
-  sed -e "s/{{KEY}}/$KEY/g" \
-    -e "s/{{TYPE}}/$TYPE/g" \
-    -e "s/{{GIVER}}/$GIVER/g" \
-    -e "s/{{WALLET}}/$WALLET_ADR/g" \
-    -e "s/{{GPU_ID}}/$GPU_ID/g" \
-    -e "s/{{BOOST_FACTOR}}/$BOOST_FACTOR/g" \
-    -e "s/{{PLATFORM_ID}}/$PLATFORM_ID/g" \
-    -e "s/{{VERBOSITY}}/$VERBOSITY/g" \
-    -e "s~{{PARAMETERS}}~$PARAMETERS~g" \
-    "$SCRIPT_DIR/assets/tonminer.service" >$UNIT_FILE
-done
 
 exit 0
--- h-run.sh
+++ h-run.sh
@@ -7,14 +7,16 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 EXEC_CONF="$SCRIPT_DIR/config/execution.config.json"
 LOGS_DIR="$SCRIPT_DIR/logs" # directory for pow-miner-gpu logs
-MINER_KEYS="$( jq -r ".keys" $EXEC_CONF )"
+MINER_KEYS=$( jq -r ".keys" $EXEC_CONF )
+TMPFS_LOGS_ENEBLED=$( jq -r ".config.tmpfs_logs_enable" $EXEC_CONF )
 
 #-------------------------------------------------------------------------
-# stop old processes
+# STOP AND REMOVE SYSTEMD UNITS
 #-------------------------------------------------------------------------
 
-systemctl daemon-reload
 systemctl list-units -t service --full | grep tonminer | awk '{print $1}' | xargs -i systemctl stop \{\}
+rm $UNITS_DIR/tonminer*.service
+systemctl daemon-reload
 rm -f $SCRIPT_DIR/*blkstate*
 
 #-------------------------------------------------------------------------
@@ -38,14 +40,43 @@ if [[ -z "$LOGS_DIR_MNT_CHECK" ]]; then
 fi
 
 #-------------------------------------------------------------------------
+# CREATE SYSTEMD UNITS
+#-------------------------------------------------------------------------
+
+echo $MINER_KEYS | jq -c -r '.[]' | while read KEY; do
+  VARS=$(echo "{$CUSTOM_USER_CONFIG}" | jq "with_entries(select([.key] | inside([\"$KEY\"]))) | .[]")
+  GIVER=$(echo $VARS | jq -r '.[0]')
+  GPU_ID=$(echo $VARS | jq -r '.[1]')
+  BOOST_FACTOR=$(echo $VARS | jq -r '.[2]')
+  PLATFORM_ID=$(echo $VARS | jq -r '.[3]')
+  VERBOSITY=$(echo $VARS | jq -r '.[4]')
+  PARAMETERS=" -s $SCRIPT_DIR/logs/status-tonminer-$KEY.json"
+  if [[ "$TMPFS_LOGS_ENEBLED" == "yes" ]]; then
+    PARAMETERS="$PARAMETERS -l $SCRIPT_DIR/logs/log-tonminer-$KEY"
+  fi
+  UNIT_FILE="$UNITS_DIR/tonminer-$KEY.service"
+  echo "INFO: create $UNIT_FILE"
+  sed -e "s/{{KEY}}/$KEY/g" \
+    -e "s/{{TYPE}}/$TYPE/g" \
+    -e "s/{{GIVER}}/$GIVER/g" \
+    -e "s/{{WALLET}}/$WALLET_ADR/g" \
+    -e "s/{{GPU_ID}}/$GPU_ID/g" \
+    -e "s/{{BOOST_FACTOR}}/$BOOST_FACTOR/g" \
+    -e "s/{{PLATFORM_ID}}/$PLATFORM_ID/g" \
+    -e "s/{{VERBOSITY}}/$VERBOSITY/g" \
+    -e "s~{{PARAMETERS}}~$PARAMETERS~g" \
+    "$SCRIPT_DIR/assets/tonminer.service" >$UNIT_FILE
+done
+
+#-------------------------------------------------------------------------
 # START MINERS
 #-------------------------------------------------------------------------
 
 echo $MINER_KEYS | jq -c -r '.[]' | while read KEY; do
-  echo "Start tonminer-$KEY.service"
+  echo "INFO: start tonminer-$KEY.service"
   systemctl start "tonminer-$KEY.service"
 done
 
 # do not exit
-echo "ALL STARTED"
+echo "INFO: ALL STARTED"
 sleep infinity
